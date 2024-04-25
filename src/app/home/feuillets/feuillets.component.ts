74943cb125a0f99a9ed4119bfc7579ea8367990d
Sechilariu <260595@sidel.com>
2024-04-25 14:50:40 +0200

    list feuillets

--- src/app/home/feuillets/feuillets.component.ts
+++ src/app/home/feuillets/feuillets.component.ts
@@ -1,12 +1,55 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import moment from 'moment';
 @Component({
   selector: 'app-feuillets',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './feuillets.component.html',
   styleUrl: './feuillets.component.scss'
 })
 export class FeuilletsComponent {
-
+  moment: any = moment;
+  feuillets: any[] = [
+    {
+      id: 0,
+      date: new Date(),
+      paroisse: {
+        nom: 'Cathédrale Saint-Maurice d\'Angers'
+      },
+      vues: 345
+    },
+    {
+      id: 1,
+      date: new Date(),
+      paroisse: {
+        nom: 'Cathédrale Saint-Maurice d\'Angers'
+      },
+      vues: 588
+    },
+    {
+      id: 2,
+      date: new Date(),
+      paroisse: {
+        nom: 'Cathédrale Saint-Maurice d\'Angers'
+      },
+      vues: 756
+    },
+    {
+      id: 3,
+      date: new Date(),
+      paroisse: {
+        nom: 'Cathédrale Saint-Maurice d\'Angers'
+      },
+      vues: 863
+    },
+    {
+      id: 4,
+      date: new Date(),
+      paroisse: {
+        nom: 'Cathédrale Saint-Maurice d\'Angers'
+      },
+      vues: 445
+    },
+  ]
 }
